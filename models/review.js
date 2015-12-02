f601effa0a08333bee9cc1e680a4107f73f3c9f4
Akshay <akshayjain1996@gmail.com>
2015-12-02 16:20:18 -0500

    Working on database

---
+++ models/review.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var reviewSchema = new mongoose.Schema({
+	id: Number,
+	userId: String.
+	review: String,
+	rating: Number
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
